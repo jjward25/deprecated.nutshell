23ab9989f41ffc72d67d5df7f0f76d361ee550ad
jjward25 <45015765+jjward25@users.noreply.github.com>
2022-03-11 15:02:56 -0500

    mobileMenu

--- components/header.js
+++ components/header.js
@@ -35,7 +35,64 @@ export default function HeaderMenu() {
         <h1 className={styles["headertitle"]}>nutshell news</h1>
       </div>
       <div className={styles[`subheader-list${rotateChevron ? "-open" : ""}`]}>
-        <MobileMenuExtension />
+        <div className={styles["mobile-menu-extension"]}>
+          <div
+            className={styles["extension-mobile"]}
+            onClick={() => mobileMenu()}
+          >
+            <span className={styles["extension-mobile-wrap"]}>
+              <Image src="/acorn.svg" alt="acorn" layout="fill" />
+            </span>
+            <div className={styles["extension-mobile-text"]}>
+              <Link href="/">News</Link>
+            </div>
+          </div>
+
+          <div
+            className={styles["extension-mobile"]}
+            onClick={() => mobileMenu()}
+          >
+            <span className={styles["extension-mobile-wrap"]}>
+              <Image src="/acorn.svg" alt="acorn" layout="fill" />
+            </span>
+            <div className={styles["extension-mobile-text"]}>
+              <Link href="/life">Life</Link>
+            </div>
+          </div>
+
+          <div
+            className={styles["extension-mobile"]}
+            onClick={() => mobileMenu()}
+          >
+            <span className={styles["extension-mobile-wrap"]}>
+              <Image src="/acorn.svg" alt="acorn" layout="fill" />
+            </span>
+            <div className={styles["extension-mobile-text"]}>
+              <Link href="/academic">Academic</Link>
+            </div>
+          </div>
+
+          <div
+            className={styles["extension-mobile"]}
+            onClick={() => mobileMenu()}
+          >
+            <span className={styles["extension-logo-wrap"]}>
+              <Image src="/acorn.svg" alt="acorn" layout="fill" />
+            </span>
+            <div className={styles["extension-mobile-text"]}>
+              <Link href="/readinglist">ReadingList</Link>
+            </div>
+          </div>
+
+          <div className={styles["extension-mobile"]}>
+            <span className={styles["extension-logo-wrap"]}>
+              <Image src="/acorn.svg" alt="acorn" layout="fill" />
+            </span>
+            <div className={styles["extension-mobile-text"]}>
+              <Link href="/about">About</Link>
+            </div>
+          </div>
+        </div>
       </div>
       <ul className={styles["headerbuttonslist"]}>
         <li className={styles["headerbuttons"]}>
--- components/mobile-menu-extension.js
+++ components/mobile-menu-extension.js
@@ -1,54 +1,11 @@
 import styles from "../styles/Components.module.scss";
 import Image from "next/image";
 import Link from "next/link";
-
+import React, { useState } from "react";
 export default function MobileMenuExtension() {
-  return (
-    <div className={styles["mobile-menu-extension"]}>
-      <div className={styles["extension-mobile"]}>
-        <span className={styles["extension-mobile-wrap"]}>
-          <Image src="/acorn.svg" alt="acorn" layout="fill" />
-        </span>
-        <div className={styles["extension-mobile-text"]}>
-          <Link href="/">News</Link>
-        </div>
-      </div>
-
-      <div className={styles["extension-mobile"]}>
-        <span className={styles["extension-mobile-wrap"]}>
-          <Image src="/acorn.svg" alt="acorn" layout="fill" />
-        </span>
-        <div className={styles["extension-mobile-text"]}>
-          <Link href="/life">Life</Link>
-        </div>
-      </div>
-
-      <div className={styles["extension-mobile"]}>
-        <span className={styles["extension-mobile-wrap"]}>
-          <Image src="/acorn.svg" alt="acorn" layout="fill" />
-        </span>
-        <div className={styles["extension-mobile-text"]}>
-          <Link href="/academic">Academic</Link>
-        </div>
-      </div>
-
-      <div className={styles["extension-mobile"]}>
-        <span className={styles["extension-logo-wrap"]}>
-          <Image src="/acorn.svg" alt="acorn" layout="fill" />
-        </span>
-        <div className={styles["extension-mobile-text"]}>
-          <Link href="/readinglist">ReadingList</Link>
-        </div>
-      </div>
-
-      <div className={styles["extension-mobile"]}>
-        <span className={styles["extension-logo-wrap"]}>
-          <Image src="/acorn.svg" alt="acorn" layout="fill" />
-        </span>
-        <div className={styles["extension-mobile-text"]}>
-          <Link href="/about">About</Link>
-        </div>
-      </div>
-    </div>
-  );
+  const [rotateChevron, setRotateChevron] = useState(false);
+  function mobileMenu() {
+    setRotateChevron(!rotateChevron);
+  }
+  return <div></div>;
 }
