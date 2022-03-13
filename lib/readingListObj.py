c3fd45c9f87ef9a37ec7dc5b50ee6650dfd1d64f
jjward25 <45015765+jjward25@users.noreply.github.com>
2022-03-13 11:36:14 -0400

    rlObjCode

--- lib/readingListObj.py
+++ lib/readingListObj.py
@@ -3,6 +3,7 @@ import json
 import pprint
 from operator import itemgetter
 pp = pprint.PrettyPrinter(indent=4)
+import operator
 
 ## Initial Excel reading and dataframe creation
 articleDF = pd.read_excel('C:/Users/Josep/OneDrive/Desktop/Coding/nutshell/public/NutshellSampleData.xlsx',engine='openpyxl')
@@ -20,7 +21,7 @@ for contentRowObj in inputObj.values():
 print(postList)
 
 ########
-# Create arrays of post objects 
+# Create arrays of Post objects
 ########
 contentObjList = []
 for post in postList:
@@ -58,48 +59,28 @@ for postObj in contentObjList:
             shDict = {}
             shDict.setdefault("SubheaderName",contentRowObj['SubheaderName'])
             shDict.setdefault("SubheaderPriority",contentRowObj['SubheaderPriority'])
+            shDict.setdefault("BulletText",contentRowObj['BulletText'])
+            shDict.setdefault("BulletPriority",contentRowObj['BulletPriority'])
+            shDict.setdefault("BulletCite",contentRowObj['BulletCite'])
+            shDict.setdefault("BulletLink",contentRowObj['BulletLink'])
+            shDict.setdefault("BulletPostDate",contentRowObj['BulletPostDate'])
+            shDict.setdefault("BulletUpDate",contentRowObj['BulletUpDate'])
             
-            if contentRowObj['SubheaderName'] not in shNames:
+            if contentRowObj['BulletText'] not in shNames:
                 shDupes.append(shDict)
-                shNames.append(contentRowObj['SubheaderName']) ## Fill list w postNames so the next time the postName comes up it's in the list and no object will be created
+                shNames.append(contentRowObj['BulletText']) ## Fill list w postNames so the next time the postName comes up it's in the list and no object will be created
             else:
                 continue
         else:
             continue
     shList = [i for n, i in enumerate(shDupes) if i not in shList[n + 1:]]  
-    shList = sorted(shList, key=itemgetter('SubheaderPriority'))  
+    ## Sort into tuples to order by subheader then bullet priorities
+    shList.sort(key = operator.itemgetter('SubheaderPriority', 'BulletPriority'))
+    print(shList)
     postObj.setdefault("SubheaderArray",shList)
 #print(postObjList)
 
-## Bullets
-for postObj in contentObjList:   
-    for shObj in postObj['SubheaderArray']:
-        bulletList = []
-        bulletDupes = []
-        bulletNames = []
-        for contentRowObj in inputObj.values(): ## Iterate through the dataset rows for a subheader match, then create the post objects to append to the post array
-            if contentRowObj['PostName']+contentRowObj['SubheaderName'] == postObj['PostName']+shObj['SubheaderName']:
-                bulletDict = {}
-                bulletDict.setdefault("BulletText",contentRowObj['BulletText'])
-                bulletDict.setdefault("BulletPriority",contentRowObj['BulletPriority'])
-                bulletDict.setdefault("BulletCite",contentRowObj['BulletCite'])
-                bulletDict.setdefault("BulletLink",contentRowObj['BulletLink'])
-                bulletDict.setdefault("BulletPostDate",contentRowObj['BulletPostDate'])
-                bulletDict.setdefault("BulletUpDate",contentRowObj['BulletUpDate'])
-                
-                if contentRowObj['BulletText'] not in bulletNames:
-                    bulletDupes.append(bulletDict)
-                    bulletNames.append(contentRowObj['BulletText']) 
-                else:
-                    continue
-            else:
-                continue
-
-        bulletList = [i for n, i in enumerate(bulletDupes) if i not in bulletList[n + 1:]]  
-        bulletList = sorted(bulletList, key=itemgetter('BulletPriority'))  
-        shObj.setdefault("BulletArray",bulletList)
-
 
 #pp.pprint(contentObjList)
-with open("postObjList.json", "w") as write_file:
+with open("readingListObjs.json", "w") as write_file:
     json.dump(contentObjList, write_file, indent=4)
