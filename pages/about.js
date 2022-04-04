75326d9e6c77d099a3f86df3b93f786177c5491a
jjward25 <45015765+jjward25@users.noreply.github.com>
2022-04-04 01:17:03 -0600

    styles

--- pages/about.js
+++ pages/about.js
@@ -5,30 +5,32 @@ export default function About() {
     <div className={styles["container"]}>
       <main className={styles["main"]}>
         <div className={styles["about-content-wrap"]}>
-          <div className={styles["about-title-header"]}>Nutshell News</div>
-          <div className={styles["font-about-header"]}>Adult Education</div>
-          <p className={styles["about-text"]}>
-            {`To us, The News should be relevant, it should be useful, and it should be easy to follow.  So we tailor our News coverage a little differently,
+          <div className={styles["about-content"]}>
+            <div className={styles["about-title-header"]}>Nutshell News</div>
+            <div className={styles["font-about-header"]}>Adult Education</div>
+            <p className={styles["about-text"]}>
+              {`To us, The News should be relevant, it should be useful, and it should be easy to follow.  So we tailor our News coverage a little differently,
               and include Life and Academic sections for you to learn about things that are more immediately useful for you than traditional news.`}
-            <br />
-            <br />
-            {`We want to make it as easy as possible to keep up with the world. That's why we publish only one article for each topic, in a simple bulleted 
+              <br />
+              <br />
+              {`We want to make it as easy as possible to keep up with the world. That's why we publish only one article for each topic, in a simple bulleted 
               format.`}
-            <br />
-            <br />
-            {`Google and Apple aggregate headlines, we aggregate information.`}
-          </p>
-          <div className={styles["font-about-header"]}>Your Reading List</div>
-          <p className={styles["about-text"]}>
-            {`To make things even easier, you can track articles so that new bullets will show up in your Reading List.  This way you read one article, once, 
+              <br />
+              <br />
+              {`Google and Apple aggregate headlines, we aggregate information.`}
+            </p>
+            <div className={styles["font-about-header"]}>Your Reading List</div>
+            <p className={styles["about-text"]}>
+              {`To make things even easier, you can track articles so that new bullets will show up in your Reading List.  This way you read one article, once, 
               then get new updates on issues you care about directly in your Reading List, without having to skim the whole article again.`}
-            <br />
-            <br />
-            {`This is smarter coverage for smarter people to promote better living.  This is Adult Education - detailed, useful information with no agenda or slant.`}
-            <br />
-            <br />
-            {`Nutshell News is news for people who hate the news.`}
-          </p>
+              <br />
+              <br />
+              {`This is smarter coverage for smarter people to promote better living.  This is Adult Education - detailed, useful information with no agenda or slant.`}
+              <br />
+              <br />
+              {`Nutshell News is news for people who hate the news.`}
+            </p>
+          </div>
         </div>
       </main>
     </div>
